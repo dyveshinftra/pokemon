b7b8bd3c64873c8855e40fe912e8fa536f6ee6ae
dyveshinftra <56993580+dyveshinftra@users.noreply.github.com>
2022-06-20 17:23:40 +0200

    refactor: move cache filename out

--- test/hd_cache_tests.erl
+++ test/hd_cache_tests.erl
@@ -18,8 +18,9 @@ cleanup({ok, _Pid}) ->
     hd_cache:stop().
 
 hd_cache_test_() ->
+    Filename = "eunit.test",
     {setup, fun setup/0, fun cleanup/1,
      [?_assert(is_list(hd_cache:get_path()) =:= true),
-      ?_assert(hd_cache:write_file("eunit.test", "eunit write test") =:= ok)
+      ?_assert(hd_cache:write_file(Filename, "eunit write test") =:= ok)
      ]
     }.

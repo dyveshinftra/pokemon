e414a5ba492c32521add1fe4494247bd605d2518
dyveshinftra <56993580+dyveshinftra@users.noreply.github.com>
2022-06-20 17:35:17 +0200

    add test that content is overwritten

--- test/hd_cache_tests.erl
+++ test/hd_cache_tests.erl
@@ -20,10 +20,12 @@ cleanup({ok, _Pid}) ->
 hd_cache_test_() ->
     Filename = "eunit.test",
     Msg1 = "eunit write test",
-    %Msg2 = "eunit double write test",
+    Msg2 = "eunit double write test",
     {setup, fun setup/0, fun cleanup/1,
      [?_assert(is_list(hd_cache:get_path()) =:= true),
       ?_assert(hd_cache:write_file(Filename, Msg1) =:= ok),
-      ?_assert(hd_cache:read_file(Filename) =:= {ok, list_to_binary(Msg1)})
+      ?_assert(hd_cache:read_file(Filename) =:= {ok, list_to_binary(Msg1)}),
+      ?_assert(hd_cache:write_file(Filename, Msg2) =:= ok),
+      ?_assert(hd_cache:read_file(Filename) =:= {ok, list_to_binary(Msg2)})
      ]
     }.

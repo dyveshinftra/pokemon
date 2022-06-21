a1fa271dd257e4af1d323e337a8d3e0b94432bc4
dyveshinftra <56993580+dyveshinftra@users.noreply.github.com>
2022-06-21 15:57:28 +0200

    15 read cache file (#16)
    * refactor: move cache filename out
    
    * hd_cache_test:read_file/1 fails
    
    * hd_cache:read_file/1 fixed
    
    * hd_cache:read_file/1 actual implementation and fix unit test
    
    * refactor cache file data
    
    * add test that content is overwritten
    
    * refactor hd_cache.erl
    
    * refactor hd_cache_tests.erl

--- test/hd_cache_tests.erl
+++ test/hd_cache_tests.erl
@@ -1,8 +1,13 @@
 -module(hd_cache_tests).
+
 -include_lib("eunit/include/eunit.hrl").
 
 -define(APPLICATION, "eunit-pt").
+-define(FILENAME,    "eunit.test").
+-define(MSG1,        "eunit write test").
+-define(MSG2,        "eunit double write test").
 
+% weird path so seperate from other tests
 configured_path_test() ->
     ConfiguredCachePath = "Yes, this is my cache path!",
     application:set_env(?APPLICATION, cache_path, ConfiguredCachePath),
@@ -11,15 +16,21 @@ configured_path_test() ->
     hd_cache:stop(),
     application:unset_env(?APPLICATION, cache_path).
 
-setup() ->
-    hd_cache:start_link(?APPLICATION).
-
-cleanup({ok, _Pid}) ->
-    hd_cache:stop().
-
+% actual API tests
+setup()             -> hd_cache:start_link(?APPLICATION).
+cleanup({ok, _Pid}) -> hd_cache:stop().
 hd_cache_test_() ->
     {setup, fun setup/0, fun cleanup/1,
-     [?_assert(is_list(hd_cache:get_path()) =:= true),
-      ?_assert(hd_cache:write_file("eunit.test", "eunit write test") =:= ok)
+     [
+      % differs per platform, just make sure it is "sane"
+      ?_assert(is_list(hd_cache:get_path()) =:= true),
+
+      % write + read test, it should match
+      ?_assert(hd_cache:write_file(?FILENAME, ?MSG1) =:= ok),
+      ?_assert(hd_cache:read_file (?FILENAME) =:= {ok, list_to_binary(?MSG1)}),
+
+      % make sure write_file "overrides"
+      ?_assert(hd_cache:write_file(?FILENAME, ?MSG2) =:= ok),
+      ?_assert(hd_cache:read_file (?FILENAME) =:= {ok, list_to_binary(?MSG2)})
      ]
     }.
